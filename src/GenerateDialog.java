954f09dbcdf2a0e59bf200e83441a0a40cacb784
Nikita Artyushov <nikita.artyushov@gmail.com>
2014-03-10 17:07:58 +0400

    Generate method implemented

---
+++ src/GenerateDialog.java
@@ -0,0 +1,28 @@
+import com.intellij.openapi.ui.DialogWrapper;
+import com.intellij.psi.PsiClass;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+/**
+ * User: nikart
+ * Date: 10/03/14
+ * Time: 16:47
+ */
+public class GenerateDialog extends DialogWrapper {
+
+//    private final JComponent centerPanel;
+
+    protected GenerateDialog(PsiClass psiClass) {
+        super(psiClass.getProject());
+        setTitle("Generate Benchmark");
+
+
+    }
+
+    @Nullable
+    @Override
+    protected JComponent createCenterPanel() {
+        return null;
+    }
+}
