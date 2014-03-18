e618086dc9dc9ac8d175eefb5c3a27d9147ca3ac
Nikita Artyushov <nikart@yandex-team.ru>
2014-03-18 14:25:11 +0400

    Benchmark method template created

---
+++ src/BenchmarkMethodTemplate.java
@@ -0,0 +1,32 @@
+import com.intellij.codeInsight.template.Expression;
+import com.intellij.codeInsight.template.Template;
+import com.intellij.codeInsight.template.TemplateManager;
+import com.intellij.codeInsight.template.impl.ConstantNode;
+import com.intellij.psi.PsiClass;
+
+/**
+ * User: nikart
+ * Date: 18/03/14
+ * Time: 13:01
+ */
+public class BenchmarkMethodTemplate {
+
+    public static Template create(PsiClass psiClass) {
+        Template template = TemplateManager.getInstance(psiClass.getProject()).createTemplate("", "");
+        template.addTextSegment("@org.openjdk.jmh.annotations.GenerateMicroBenchmark\n");
+        template.addTextSegment("public ");
+        Expression typeExpr = new ConstantNode("void");
+        template.addVariable("type", typeExpr, typeExpr, true);
+
+        template.addTextSegment(" measure");
+        Expression nameExpr = new ConstantNode("Name");
+        template.addVariable("name", nameExpr, nameExpr, true);
+        template.addTextSegment("() {\n\n}");
+
+        template.setToIndent(true);
+        template.setToReformat(true);
+        template.setToShortenLongNames(true);
+
+        return template;
+    }
+}
--- src/GenerateMicroBenchmarkAction.java
+++ src/GenerateMicroBenchmarkAction.java
@@ -1,12 +1,24 @@
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.LangDataKeys;
-import com.intellij.openapi.actionSystem.PlatformDataKeys;
-import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.codeInsight.CodeInsightUtilCore;
+import com.intellij.codeInsight.daemon.impl.quickfix.CreateFromUsageUtils;
+import com.intellij.codeInsight.generation.GenerateMembersUtil;
+import com.intellij.codeInsight.generation.OverrideImplementUtil;
+import com.intellij.codeInsight.generation.PsiGenerationInfo;
+import com.intellij.codeInsight.template.Template;
+import com.intellij.codeInsight.template.TemplateEditingAdapter;
+import com.intellij.codeInsight.template.TemplateManager;
+import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.*;
-import com.intellij.psi.codeStyle.JavaCodeStyleManager;
 import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.psi.util.PsiUtilBase;
+import com.intellij.testIntegration.TestIntegrationUtils;
+import com.intellij.util.IncorrectOperationException;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collections;
 
 /**
  * User: nikart
@@ -17,32 +29,79 @@ public class GenerateMicroBenchmarkAction extends AnAction {
 
     @Override
     public void actionPerformed(AnActionEvent e) {
+        DataContext dataContext = e.getDataContext();
+        final Project project = CommonDataKeys.PROJECT.getData(dataContext);
+        if (project == null) {
+            return;
+        }
+        final Editor editor = CommonDataKeys.EDITOR.getData(dataContext);
+        if (editor == null) {
+            return;
+        }
+        PsiFile psiFile = PsiUtilBase.getPsiFileInEditor(editor, project);
         PsiClass psiClass = getPsiClassFromContext(e);
-        generateMethod(psiClass);
+        Template template = BenchmarkMethodTemplate.create(psiClass);
+        PsiMethod method = generateDummyMethod(editor, psiFile);
+        if (method == null) {
+            return;
+        }
+        final TextRange range = method.getTextRange();
+        editor.getDocument().replaceString(range.getStartOffset(), range.getEndOffset(), "");
+        editor.getCaretModel().moveToOffset(range.getStartOffset());
 
-//        GenerateDialog dlg = new GenerateDialog(psiClass);
-//        dlg.show();
-//        if (dlg.isOK()) {
-//
-//        }
-    }
-
-    @Override
-    public void update(AnActionEvent e) {
-        PsiClass psiClass = getPsiClassFromContext(e);
-        e.getPresentation().setEnabled(psiClass != null);
-    }
-
-    private void generateMethod(final PsiClass psiClass) {
-        new WriteCommandAction.Simple(psiClass.getProject(), psiClass.getContainingFile()) {
+        TemplateEditingAdapter adapter = new TemplateEditingAdapter() {
             @Override
-            protected void run() throws Throwable {
-                PsiElementFactory elementFactory = JavaPsiFacade.getElementFactory(psiClass.getProject());
-                PsiMethod method = elementFactory.createMethodFromText(generateBenchmarkMethodString(), psiClass);
-                PsiElement element = psiClass.add(method);
-                JavaCodeStyleManager.getInstance(psiClass.getProject()).shortenClassReferences(element);
+            public void templateFinished(Template template, boolean brokenOff) {
+                ApplicationManager.getApplication().runWriteAction(new Runnable() {
+                    public void run() {
+                        PsiDocumentManager.getInstance(project).commitDocument(editor.getDocument());
+                        PsiFile psi = PsiDocumentManager.getInstance(project).getPsiFile(editor.getDocument());
+                        if (psi == null) {
+                            return;
+                        }
+                        PsiElement el = PsiTreeUtil.findElementOfClassAtOffset(psi, editor.getCaretModel().getOffset() - 1, PsiMethod.class, false);
+
+                        if (el != null) {
+                            PsiMethod method = PsiTreeUtil.getParentOfType(el, PsiMethod.class, false);
+                            if (method != null) {
+                                if (method.findDeepestSuperMethods().length > 0) {
+                                    GenerateMembersUtil.setupGeneratedMethod(method);
+                                }
+                                CreateFromUsageUtils.setupEditor(method, editor);
+                            }
+                        }
+                    }
+                });
+
             }
-        }.execute();
+        };
+
+        TemplateManager.getInstance(project).startTemplate(editor, template, adapter);
+    }
+
+    @Nullable
+    private static PsiMethod generateDummyMethod(Editor editor, PsiFile file) throws IncorrectOperationException {
+        final PsiMethod method = TestIntegrationUtils.createDummyMethod(file);
+        final PsiGenerationInfo<PsiMethod> info = OverrideImplementUtil.createGenerationInfo(method);
+
+        int offset = findOffsetToInsertMethodTo(editor, file);
+        GenerateMembersUtil.insertMembersAtOffset(file, offset, Collections.singletonList(info));
+
+        final PsiMethod member = info.getPsiMember();
+        return member != null ? CodeInsightUtilCore.forcePsiPostprocessAndRestoreElement(member) : null;
+    }
+
+    private static int findOffsetToInsertMethodTo(Editor editor, PsiFile file) {
+        int result = editor.getCaretModel().getOffset();
+
+        PsiClass classAtCursor = PsiTreeUtil.getParentOfType(file.findElementAt(result), PsiClass.class, false);
+
+        while (classAtCursor != null && !(classAtCursor.getParent() instanceof PsiFile)) {
+            result = classAtCursor.getTextRange().getEndOffset();
+            classAtCursor = PsiTreeUtil.getParentOfType(classAtCursor, PsiClass.class);
+        }
+
+        return result;
     }
 
     private PsiClass getPsiClassFromContext(AnActionEvent e) {
@@ -53,18 +112,6 @@ public class GenerateMicroBenchmarkAction extends AnAction {
         }
         int offset = editor.getCaretModel().getOffset();
         PsiElement elementAt = psiFile.findElementAt(offset);
-        PsiClass psiClass = PsiTreeUtil.getParentOfType(elementAt, PsiClass.class);
-        return psiClass;
-    }
-
-    private String generateBenchmarkMethodString() {
-        StringBuilder builder = new StringBuilder();
-        builder
-                .append("@GenerateMicroBenchmark\n")
-                .append("public void benchmarkName() {\n")
-                .append("\n")
-                .append("}\n");
-        return builder.toString();
-
+        return PsiTreeUtil.getParentOfType(elementAt, PsiClass.class);
     }
 }
