8a93f0f505558ded8bb4efcb0231e34125d037c9
Nikita Artyushov <nikart@yandex-team.ru>
2014-08-11 15:25:27 +0400

    #8 invoking jmh Main directly

--- src/ru/artyushov/jmhPlugin/configuration/BenchmarkState.java
+++ src/ru/artyushov/jmhPlugin/configuration/BenchmarkState.java
@@ -3,16 +3,17 @@ package ru.artyushov.jmhPlugin.configuration;
 import com.intellij.compiler.CompilerConfiguration;
 import com.intellij.compiler.CompilerConfigurationImpl;
 import com.intellij.execution.ExecutionException;
-import com.intellij.execution.configurations.JavaCommandLine;
-import com.intellij.execution.configurations.JavaCommandLineState;
-import com.intellij.execution.configurations.JavaParameters;
+import com.intellij.execution.configurations.*;
+import com.intellij.execution.process.ProcessHandler;
 import com.intellij.execution.runners.ExecutionEnvironment;
 import com.intellij.execution.util.JavaParametersUtil;
+import com.intellij.ide.DataManager;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.projectRoots.ex.JavaSdkUtil;
 import com.intellij.openapi.roots.ModuleRootManager;
 import com.intellij.openapi.roots.ProjectRootManager;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.jps.model.java.compiler.ProcessorConfigProfile;
 
 /**
@@ -20,7 +21,7 @@ import org.jetbrains.jps.model.java.compiler.ProcessorConfigProfile;
  * Date: 14/07/14
  * Time: 21:36
  */
-public class BenchmarkState extends JavaCommandLineState implements JavaCommandLine {
+public class BenchmarkState extends CommandLineState {
 
     private final Project project;
     private final JmhConfiguration configuration;
@@ -39,7 +40,6 @@ public class BenchmarkState extends JavaCommandLineState implements JavaCommandL
         }
     }
 
-    @Override
     protected JavaParameters createJavaParameters() throws ExecutionException {
 
         JavaParameters parameters = new JavaParameters();
@@ -47,8 +47,8 @@ public class BenchmarkState extends JavaCommandLineState implements JavaCommandL
 
         parameters.setMainClass(JmhConfiguration.JMH_START_CLASS);
 
-        int classPathType = JavaParametersUtil.getClasspathType(configuration.getConfigurationModule(),
-                JmhConfiguration.JMH_START_CLASS, true);
+        int classPathType = removeJdkClasspath(JavaParametersUtil.getClasspathType(configuration.getConfigurationModule(),
+                JmhConfiguration.JMH_START_CLASS, true));
         JavaParametersUtil.configureModule(configuration.getConfigurationModule(), parameters, classPathType, null);
 
         Module module = configuration.getConfigurationModule().getModule();
@@ -57,8 +57,29 @@ public class BenchmarkState extends JavaCommandLineState implements JavaCommandL
                     ? ModuleRootManager.getInstance(module).getSdk()
                     : ProjectRootManager.getInstance(project).getProjectSdk());
         }
-        parameters.getClassPath().add(JavaSdkUtil.getIdeaRtJarPath());
-
         return parameters;
+    }
+
+    @NotNull
+    @Override
+    protected ProcessHandler startProcess() throws ExecutionException {
+        return JavaCommandLineStateUtil.startProcess(createCommandLine(), false);
+    }
+
+    private GeneralCommandLine createCommandLine() throws ExecutionException {
+        return CommandLineBuilder.createFromJavaParameters(createJavaParameters(), CommonDataKeys.PROJECT
+                .getData(DataManager.getInstance().getDataContext()), true);
+    }
+
+
+    private int removeJdkClasspath(int classpathType) {
+        switch (classpathType) {
+            case JavaParameters.JDK_AND_CLASSES:
+                return JavaParameters.CLASSES_ONLY;
+            case JavaParameters.JDK_AND_CLASSES_AND_TESTS:
+                return JavaParameters.CLASSES_AND_TESTS;
+            default:
+                return classpathType;
+        }
     }
 }
