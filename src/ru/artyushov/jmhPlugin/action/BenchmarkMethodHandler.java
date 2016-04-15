bf4bced1120e55aebdc3d21668e089f227591c80
artyushov <nikita.artyushov@gmail.com>
2016-04-16 00:25:41 +0300

    #11 - Refactored generate action, got rid of npe.

---
+++ src/ru/artyushov/jmhPlugin/action/BenchmarkMethodHandler.java
@@ -0,0 +1,151 @@
+package ru.artyushov.jmhPlugin.action;
+
+import com.intellij.codeInsight.CodeInsightActionHandler;
+import com.intellij.codeInsight.CodeInsightUtilCore;
+import com.intellij.codeInsight.daemon.impl.quickfix.CreateFromUsageUtils;
+import com.intellij.codeInsight.generation.GenerateMembersUtil;
+import com.intellij.codeInsight.generation.OverrideImplementUtil;
+import com.intellij.codeInsight.generation.PsiGenerationInfo;
+import com.intellij.codeInsight.template.Template;
+import com.intellij.codeInsight.template.TemplateEditingAdapter;
+import com.intellij.codeInsight.template.TemplateManager;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.command.CommandProcessor;
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.actionSystem.DocCommandGroupId;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.*;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.testIntegration.TestIntegrationUtils;
+import com.intellij.util.IncorrectOperationException;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collections;
+
+/**
+ * Author: artyushov
+ * Date: 2016-04-16 00:16
+ */
+class BenchmarkMethodHandler implements CodeInsightActionHandler {
+
+    @Override
+    public void invoke(@NotNull Project project, @NotNull final Editor editor, @NotNull final PsiFile file) {
+        PsiClass targetClass = findTargetClass(editor, file);
+        if (targetClass == null) {
+            return;
+        }
+        generate(editor, file, targetClass);
+    }
+
+    @Override
+    public boolean startInWriteAction() {
+        return false;
+    }
+
+    private void generate(final Editor editor, final PsiFile file, final PsiClass targetClass) {
+
+        WriteCommandAction.runWriteCommandAction(file.getProject(), new Runnable() {
+            @Override
+            public void run() {
+                PsiDocumentManager.getInstance(file.getProject()).commitAllDocuments();
+                PsiMethod method = generateDummyMethod(editor, file);
+                if (method == null) {
+                    return;
+                }
+
+                Template template = BenchmarkMethodTemplateFactory.create(targetClass);
+                createSpaceForNewMethod(file.getProject(), editor, file);
+                TemplateEditingAdapter adapter = createTemplateAdapter(new Runnable() {
+                    public void run() {
+
+                        PsiDocumentManager.getInstance(file.getProject()).commitDocument(editor.getDocument());
+                        PsiFile psi = PsiDocumentManager.getInstance(file.getProject()).getPsiFile(editor.getDocument());
+                        if (psi == null) {
+                            return;
+                        }
+                        PsiElement el = PsiTreeUtil.findElementOfClassAtOffset(psi, editor.getCaretModel().getOffset() - 1, PsiMethod.class, false);
+
+                        if (el == null) {
+                            return;
+                        }
+
+                        PsiMethod method = PsiTreeUtil.getParentOfType(el, PsiMethod.class, false);
+                        if (method == null) {
+                            return;
+                        }
+
+                        if (method.findDeepestSuperMethods().length > 0) {
+                            GenerateMembersUtil.setupGeneratedMethod(method);
+                        }
+                        CreateFromUsageUtils.setupEditor(method, editor);
+                    }
+                });
+                TemplateManager.getInstance(file.getProject()).startTemplate(editor, template, adapter);
+            }
+        });
+    }
+
+    private static PsiClass findTargetClass(@NotNull Editor editor, @NotNull PsiFile file) {
+        int offset = editor.getCaretModel().getOffset();
+        PsiElement element = file.findElementAt(offset);
+        return PsiTreeUtil.getParentOfType(element, PsiClass.class, false) == null ? null : TestIntegrationUtils.findOuterClass(element);
+    }
+
+    private static PsiMethod generateDummyMethod(Editor editor, PsiFile file) throws IncorrectOperationException {
+        PsiMethod method = TestIntegrationUtils.createDummyMethod(file);
+        PsiGenerationInfo<PsiMethod> info = OverrideImplementUtil.createGenerationInfo(method);
+
+        int offset = findOffsetToInsertMethodTo(editor, file);
+        GenerateMembersUtil.insertMembersAtOffset(file, offset, Collections.singletonList(info));
+
+        PsiMethod member = info.getPsiMember();
+        return member != null ? CodeInsightUtilCore.forcePsiPostprocessAndRestoreElement(member) : null;
+    }
+
+    private static void createSpaceForNewMethod(Project project, final Editor editor, final PsiFile psiFile) {
+        CommandProcessor.getInstance().executeCommand(project, new Runnable() {
+            @Override
+            public void run() {
+
+                ApplicationManager.getApplication().runWriteAction(new Runnable() {
+                    @Override
+                    public void run() {
+                        PsiMethod method = generateDummyMethod(editor, psiFile);
+                        if (method == null) {
+                            return;
+                        }
+
+                        TextRange range = method.getTextRange();
+                        editor.getDocument().replaceString(range.getStartOffset(), range.getEndOffset(), "");
+                        editor.getCaretModel().moveToOffset(range.getStartOffset());
+                    }
+                });
+
+            }
+        }, "", DocCommandGroupId.noneGroupId(editor.getDocument()));
+    }
+
+    private static TemplateEditingAdapter createTemplateAdapter(final Runnable runnable) {
+        return new TemplateEditingAdapter() {
+            @Override
+            public void templateFinished(Template template, boolean brokenOff) {
+                ApplicationManager.getApplication().runWriteAction(runnable);
+            }
+        };
+    }
+
+    private static int findOffsetToInsertMethodTo(Editor editor, PsiFile file) {
+        int result = editor.getCaretModel().getOffset();
+
+        PsiClass classAtCursor = PsiTreeUtil.getParentOfType(file.findElementAt(result), PsiClass.class, false);
+
+        while (classAtCursor != null && !(classAtCursor.getParent() instanceof PsiFile)) {
+            result = classAtCursor.getTextRange().getEndOffset();
+            classAtCursor = PsiTreeUtil.getParentOfType(classAtCursor, PsiClass.class);
+        }
+
+        return result;
+    }
+}
--- src/ru/artyushov/jmhPlugin/action/GenerateMicroBenchmarkAction.java
+++ src/ru/artyushov/jmhPlugin/action/GenerateMicroBenchmarkAction.java
@@ -1,142 +1,15 @@
 package ru.artyushov.jmhPlugin.action;
 
-import com.intellij.codeInsight.CodeInsightUtilCore;
-import com.intellij.codeInsight.daemon.impl.quickfix.CreateFromUsageUtils;
-import com.intellij.codeInsight.generation.GenerateMembersUtil;
-import com.intellij.codeInsight.generation.OverrideImplementUtil;
-import com.intellij.codeInsight.generation.PsiGenerationInfo;
-import com.intellij.codeInsight.template.Template;
-import com.intellij.codeInsight.template.TemplateEditingAdapter;
-import com.intellij.codeInsight.template.TemplateManager;
-import com.intellij.openapi.actionSystem.*;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.command.CommandProcessor;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.actionSystem.DocCommandGroupId;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.TextRange;
-import com.intellij.psi.*;
-import com.intellij.psi.util.PsiTreeUtil;
-import com.intellij.psi.util.PsiUtilBase;
-import com.intellij.testIntegration.TestIntegrationUtils;
-import com.intellij.util.IncorrectOperationException;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.Collections;
+import com.intellij.codeInsight.generation.actions.BaseGenerateAction;
 
 /**
  * User: nikart
  * Date: 10/03/14
  * Time: 16:43
  */
-public class GenerateMicroBenchmarkAction extends AnAction {
+public class GenerateMicroBenchmarkAction extends BaseGenerateAction {
 
-    @Override
-    public void actionPerformed(AnActionEvent e) {
-        DataContext dataContext = e.getDataContext();
-        final Project project = CommonDataKeys.PROJECT.getData(dataContext);
-        if (project == null) {
-            return;
-        }
-        final Editor editor = CommonDataKeys.EDITOR.getData(dataContext);
-        if (editor == null) {
-            return;
-        }
-        final PsiFile psiFile = PsiUtilBase.getPsiFileInEditor(editor, project);
-        PsiClass psiClass = getPsiClassFromContext(e);
-        Template template = BenchmarkMethodTemplateFactory.create(psiClass);
-        createSpaceForNewMethod(project, editor, psiFile);
-        TemplateEditingAdapter adapter = createTemplateApapter(new Runnable() {
-            public void run() {
-
-                PsiDocumentManager.getInstance(project).commitDocument(editor.getDocument());
-                PsiFile psi = PsiDocumentManager.getInstance(project).getPsiFile(editor.getDocument());
-                if (psi == null) {
-                    return;
-                }
-                PsiElement el = PsiTreeUtil.findElementOfClassAtOffset(psi, editor.getCaretModel().getOffset() - 1, PsiMethod.class, false);
-
-                if (el == null) {
-                    return;
-                }
-                PsiMethod method = PsiTreeUtil.getParentOfType(el, PsiMethod.class, false);
-                if (method == null) {
-                    return;
-                }
-                if (method.findDeepestSuperMethods().length > 0) {
-                    GenerateMembersUtil.setupGeneratedMethod(method);
-                }
-                CreateFromUsageUtils.setupEditor(method, editor);
-            }
-        });
-        TemplateManager.getInstance(project).startTemplate(editor, template, adapter);
-    }
-
-    private TemplateEditingAdapter createTemplateApapter(final Runnable runnable) {
-        return new TemplateEditingAdapter() {
-            @Override
-            public void templateFinished(Template template, boolean brokenOff) {
-                ApplicationManager.getApplication().runWriteAction(runnable);
-            }
-        };
-    }
-
-    @Nullable
-    private static PsiMethod generateDummyMethod(Editor editor, PsiFile file) throws IncorrectOperationException {
-        final PsiMethod method = TestIntegrationUtils.createDummyMethod(file);
-        final PsiGenerationInfo<PsiMethod> info = OverrideImplementUtil.createGenerationInfo(method);
-
-        int offset = findOffsetToInsertMethodTo(editor, file);
-        GenerateMembersUtil.insertMembersAtOffset(file, offset, Collections.singletonList(info));
-
-        final PsiMethod member = info.getPsiMember();
-        return member != null ? CodeInsightUtilCore.forcePsiPostprocessAndRestoreElement(member) : null;
-    }
-
-    private static int findOffsetToInsertMethodTo(Editor editor, PsiFile file) {
-        int result = editor.getCaretModel().getOffset();
-
-        PsiClass classAtCursor = PsiTreeUtil.getParentOfType(file.findElementAt(result), PsiClass.class, false);
-
-        while (classAtCursor != null && !(classAtCursor.getParent() instanceof PsiFile)) {
-            result = classAtCursor.getTextRange().getEndOffset();
-            classAtCursor = PsiTreeUtil.getParentOfType(classAtCursor, PsiClass.class);
-        }
-
-        return result;
-    }
-
-    private void createSpaceForNewMethod(Project project, final Editor editor, final PsiFile psiFile) {
-        CommandProcessor.getInstance().executeCommand(project, new Runnable() {
-            @Override
-            public void run() {
-
-                ApplicationManager.getApplication().runWriteAction(new Runnable() {
-                    @Override
-                    public void run() {
-                        final PsiMethod method = generateDummyMethod(editor, psiFile);
-                        if (method == null) {
-                            return;
-                        }
-
-                        TextRange range = method.getTextRange();
-                        editor.getDocument().replaceString(range.getStartOffset(), range.getEndOffset(), "");
-                        editor.getCaretModel().moveToOffset(range.getStartOffset());
-                    }
-                });
-
-            }
-        }, "", DocCommandGroupId.noneGroupId(editor.getDocument()));
-    }
-
-    private PsiClass getPsiClassFromContext(AnActionEvent e) {
-        PsiFile psiFile = e.getData(LangDataKeys.PSI_FILE);
-        Editor editor = e.getData(PlatformDataKeys.EDITOR);
-        if (psiFile == null || editor == null) {
-            return null;
-        }
-        int offset = editor.getCaretModel().getOffset();
-        PsiElement elementAt = psiFile.findElementAt(offset);
-        return PsiTreeUtil.getParentOfType(elementAt, PsiClass.class);
+    public GenerateMicroBenchmarkAction() {
+        super(new BenchmarkMethodHandler());
     }
 }
