ce3810d13ed0672004b38c8b6be5da31272c6877
Sergey Ponomarev <stokito@gmail.com>
2020-10-14 15:53:12 +0300

    Merge JmhConfigurationProducer into JmhClassConfigurationProducer

--- src/ru/artyushov/jmhPlugin/configuration/producer/JmhClassConfigurationProducer.java
+++ src/ru/artyushov/jmhPlugin/configuration/producer/JmhClassConfigurationProducer.java
@@ -1,7 +1,10 @@
 package ru.artyushov.jmhPlugin.configuration.producer;
 
+import com.intellij.execution.JavaExecutionUtil;
 import com.intellij.execution.Location;
 import com.intellij.execution.actions.ConfigurationContext;
+import com.intellij.execution.configurations.ConfigurationFactory;
+import com.intellij.execution.junit.JavaRunConfigurationProducerBase;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.util.Ref;
 import com.intellij.psi.PsiClass;
@@ -12,6 +15,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import ru.artyushov.jmhPlugin.configuration.ConfigurationUtils;
 import ru.artyushov.jmhPlugin.configuration.JmhConfiguration;
+import ru.artyushov.jmhPlugin.configuration.JmhConfigurationType;
 
 import java.util.Iterator;
 
@@ -19,15 +23,28 @@ import static ru.artyushov.jmhPlugin.configuration.JmhConfiguration.Type.CLASS;
 import static ru.artyushov.jmhPlugin.configuration.JmhConfiguration.Type.METHOD;
 
 /**
- * User: nikart
- * Date: 15/07/14
- * Time: 23:30
+ * Supports creating run configurations from context (by right-clicking a code element in the source editor or the project view).
  */
-public class JmhClassConfigurationProducer extends JmhConfigurationProducer {
+public class JmhClassConfigurationProducer extends JavaRunConfigurationProducerBase<JmhConfiguration> implements Cloneable {
 
+    @NotNull
     @Override
-    protected boolean setupConfigurationFromContext(JmhConfiguration configuration, ConfigurationContext context,
-                                                    Ref<PsiElement> sourceElement) {
+    public ConfigurationFactory getConfigurationFactory() {
+        return JmhConfigurationType.getInstance().getConfigurationFactories()[0];
+    }
+
+    /**
+     * Sets up a configuration based on the specified context.
+     *
+     * @param configuration a clone of the template run configuration of the specified type
+     * @param context       contains the information about a location in the source code.
+     * @param sourceElement a reference to the source element for the run configuration (by default contains the element at caret,
+     *                      can be updated by the producer to point to a higher-level element in the tree).
+     * @return true if the context is applicable to this run configuration producer, false if the context is not applicable and the
+     * configuration should be discarded.
+     */
+    @Override
+    protected boolean setupConfigurationFromContext(JmhConfiguration configuration, ConfigurationContext context, Ref<PsiElement> sourceElement) {
         JmhConfiguration.Type runType;
         PsiClass benchmarkClass;
         PsiMethod method = ConfigurationUtils.getAnnotatedMethod(context);
@@ -55,6 +72,13 @@ public class JmhClassConfigurationProducer extends JmhConfigurationProducer {
         return true;
     }
 
+    /**
+     * Checks if the specified configuration was created from the specified context.
+     *
+     * @param configuration a configuration instance.
+     * @param context       contains the information about a location in the source code.
+     * @return true if this configuration was created from the specified context, false otherwise.
+     */
     @Override
     public boolean isConfigurationFromContext(JmhConfiguration configuration, ConfigurationContext context) {
         PsiClass benchmarkClass;
@@ -79,7 +103,22 @@ public class JmhClassConfigurationProducer extends JmhConfigurationProducer {
             return false;
         }
         String configurationName = getNameForConfiguration(benchmarkClass, method);
-        return isConfigurationFromContext(configuration, context, configurationName);
+        if (!configuration.getName().equals(configurationName)) {
+            return false;
+        }
+        Location locationFromContext = context.getLocation();
+        if (locationFromContext == null) {
+            return false;
+        }
+        Location location = JavaExecutionUtil.stepIntoSingleClass(locationFromContext);
+        final Module originalModule = configuration.getConfigurationModule().getModule();
+        if (location.getModule() == null || !location.getModule().equals(originalModule)) {
+            return false;
+        }
+        setupConfigurationModule(context, configuration);
+        configuration.restoreOriginalModule(originalModule);
+
+        return true;
     }
 
     private PsiClass getBenchmarkClass(ConfigurationContext context) {
@@ -108,5 +147,9 @@ public class JmhClassConfigurationProducer extends JmhConfigurationProducer {
             return benchmarkClass.getQualifiedName() + ".*";
         }
         return benchmarkClass.getQualifiedName() + '.' + method.getName();
+    }
+
+    String createProgramParameters(String generatedParams, String defaultParams) {
+        return defaultParams != null ? generatedParams + ' ' + defaultParams : generatedParams;
     }
 }
