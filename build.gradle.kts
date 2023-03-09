8bf28c2659d9d5d35d9b5110019faf8e23cd5d5a
Sergey Ponomarev <stokito@gmail.com>
2023-03-09 17:15:11 +0200

    Add gradle plugin

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    id("java")
+    id("org.jetbrains.intellij") version "1.13.1"
+}
+
+group = "com.github.artyushov.idea-jmh-plugin"
+version = "1.4.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+// Configure Gradle IntelliJ Plugin
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
+intellij {
+    version.set("2022.3.1")
+    type.set("IC") // Target IDE Platform
+
+    plugins.set(listOf("java"))
+}
+
+tasks {
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
+
+    patchPluginXml {
+        sinceBuild.set("223")
+//        untilBuild.set("231.*")
+    }
+
+    signPlugin {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
+
+    publishPlugin {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
+}
